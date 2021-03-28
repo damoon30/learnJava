f6dc7c9f687dee818a123356d893aee26338051f
fangwen <17343100344@163.com>
2021-03-28 22:17:25 +0800

    项目提交

---
+++ gmall-user-service/src/main/java/com/ayguigu/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,12 @@
+package com.ayguigu.gmall.user.mapper;
+
+
+
+
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+
+
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/gmalluser/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.gmalluser.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+
+
+}
